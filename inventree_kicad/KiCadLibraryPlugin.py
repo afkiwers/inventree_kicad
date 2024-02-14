bd0226d3bd5edf9416b01c8b680bbf398627ca67
afkiwers <andre@currawong.aero>
2024-02-14 13:56:33 +1100

    style fix

--- inventree_kicad/KiCadLibraryPlugin.py
+++ inventree_kicad/KiCadLibraryPlugin.py
@@ -59,9 +59,8 @@ class KiCadLibraryPlugin(UrlsMixin, AppMixin, SettingsMixin, SettingsContentMixi
         },
         'KICAD_INCLUDE_IPN': {
             'name': _('Include IPN'),
-             'description': _(
-                'When activated, the IPN is included in the KiCad fields for a part'),
-            'choices': [('0', 'Do not Include'), ('False', 'Include but Hide in Schematic'),  ('True', 'Include and Show in Schematic')],
+             'description': _('When activated, the IPN is included in the KiCad fields for a part'),
+            'choices': [('0', 'Do not Include'), ('False', 'Include but Hide in Schematic'), ('True', 'Include and Show in Schematic')],
             'default': '0',
         },
         'KICAD_META_DATA_IMPORT_ADD_DATASHEET': {
